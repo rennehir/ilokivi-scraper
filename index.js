3a56cd3f22a70aebffddddf931cbf624d96afd7f
rennehir <git@rennehir.fi>
2019-04-16 15:30:52 +0300

    Try to fix

--- index.js
+++ index.js
@@ -1,65 +1,72 @@
-const express = require('express')
-const axios = require('axios')
-const cheerio = require('cheerio')
+const express = require("express");
+const axios = require("axios");
+const cheerio = require("cheerio");
 
-const ILOKIVI_URL = 'https://www.ilokivi.fi/ravintola/lounas'
+const ILOKIVI_URL = "https://www.ilokivi.fi/ravintola/lounas";
 
-const app = express()
+const app = express();
 
-app.get('/lunch/today', async (req, res) => {
-    res.set('Content-Type', 'application/json; charset=utf-8')
+app.get("/lunch/today", async (req, res) => {
+  res.set("Content-Type", "application/json; charset=utf-8");
 
-    try {
-        const { data: htmlString } = await axios(ILOKIVI_URL)
-        const $ = cheerio.load(htmlString, {
-            decodeEntities: false
-        })
+  try {
+    const { data: htmlString } = await axios(ILOKIVI_URL);
+    const $ = cheerio.load(htmlString, {
+      decodeEntities: false
+    });
 
-        const day = $('.block-lunch-wrapper .col-lunch .lunch-box .head h3').text()
-        const lunchContent = $('.block-lunch-wrapper .col-lunch .lunch-box .content p').html()
-        const lunchArray = lunchContent.trim().replace(/\n/, '').split('<br>')
-        const lunches = lunchArray.map(lunch => {
-            const [dish, allergens] = lunch.replace('</i>', '').split(' <i>')
-            return { dish, allergens }
-        })
+    const day = $(".block-lunch-wrapper .col-lunch .lunch-box .head h3").text();
+    const lunchContent = $(
+      ".block-lunch-wrapper .col-lunch .lunch-box .content p"
+    ).html();
+    const lunchArray = lunchContent
+      .trim()
+      .replace(/\n/, "")
+      .split("<br>");
+    const lunches = lunchArray.map(lunch => {
+      const [dish, allergens] = lunch.replace("</i>", "").split(" <i>");
+      return { dish, allergens };
+    });
 
-        if (req.query.semmaFormat) {
-            const LunchTime = $('.lunch-time').text()
-            const now = new Date()
-            const date = now.toISOString()
+    if (req.query.semmaFormat) {
+      const LunchTime = $(".lunch-time").text();
+      const now = new Date();
+      const date = now.toISOString();
 
-            const SetMenus = lunches.map(lunch => {
-                return {
-                    Name: 'LOUNAS',
-                    Price: '2,60',
-                    Components: [`${lunch.dish} (${lunch.allergens})`]
-                }
-            })
+      const SetMenus = lunches.map(lunch => {
+        return {
+          Name: "LOUNAS",
+          Price: "2,60",
+          Components: [`${lunch.dish} (${lunch.allergens})`]
+        };
+      });
 
-            res.json({
-                RestaurantName: 'Ravintola Ilokivi',
-                MenusForDays: [
-                    {
-                        Date: date,
-                        LunchTime,
-                        SetMenus
-                    }
-                ]
-            })
-        } else {
-            res.json({
-                day,
-                lunches
-            })
-        }
-    } catch (error) {
-        console.log(error)
-        res.json({
-            error
-        })
+      res.json({
+        RestaurantName: "Ravintola Ilokivi",
+        MenusForDays: [
+          {
+            Date: date,
+            LunchTime,
+            SetMenus
+          }
+        ]
+      });
+    } else {
+      res.json({
+        day,
+        lunches
+      });
     }
-})
+  } catch (error) {
+    console.log(error);
+    res.json({
+      error
+    });
+  }
+});
 
 app.listen(3000, () => {
-    console.log()
-})
+  console.log();
+});
+
+module.exports = app;
